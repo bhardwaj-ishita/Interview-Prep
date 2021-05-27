186f493ffbf8fa4eea6ed0cf722f0533af508e47
Ishita <bhardwajishita15@gmail.com>
2021-05-27 21:11:34 +0530

    documented the questions

--- DSA/LinkedList/src/MidNumber.java
+++ DSA/LinkedList/src/MidNumber.java
@@ -1,10 +1,26 @@
+/**
+ * <h1>Middle of a linked list</>
+ * https://leetcode.com/problems/middle-of-the-linked-list/
+ * Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+ * If there are two middle nodes, return the second middle node.
+ *
+ * @author Ishita Bhardwaj
+ * @version 11.01
+ * @since   2021-05-27
+ * */
 public class MidNumber {
 
-    /**
-     * space O(1)
-     * time O(n)
-     * */
-    public SLL.Node method1(SLL.Node head) {
+    public static SLL.Node method1(SLL.Node head) {
+        /**
+         * This method has two pointers, slow and fast.
+         * slow goes one node at a time whereas fast skips a node at a time.
+         * fast = 2 * slow
+         * So when fast reaches the end of the list, slow would be in the middle of the list
+         * space complexity: O(1)
+         * time complexity: O(n)
+         * @param (SLL.Node head) The parameter is used to point at the head of an input linked list
+         * @return SLL.Node returns the Node in the middle of the linked list.
+         */
         SLL.Node slow = head, fast = head;
 
         while(fast.next != null && slow != null) {
@@ -14,11 +30,18 @@ public class MidNumber {
         return slow;
     }
 
-    /**
-     * space O(n)
-     * time O(n)
-     * */
-    public SLL.Node method2(SLL.Node head) {
+
+    public static SLL.Node method2(SLL.Node head) {
+        /**
+         * This method has a Node Array with max size
+         * A temporary pointer is pointed at the head
+         * In the while loop, as we traverse the linked list, we save the pointers in the Node Array until
+         * we've completed the list
+         * space complexity: O(n)
+         * time complexity: O(n)
+         * @param (SLL.Node head) The parameter is used to point at the head of a linked list
+         * @return SLL.Node[i/2] returns the Node in the middle of the linked list, where i is the size of the list
+         */
         SLL.Node[] A = new SLL.Node[100];
         SLL.Node temp = head;
         int i = 0;
@@ -26,8 +49,22 @@ public class MidNumber {
             A[i++] = temp;
             temp = temp.next;
         }
-
         return A[i/2];
+    }
+
+    /**
+     * Driver code
+     * */
+    public static void main(String[] args) {
+        SLL sll = new SLL();
+
+        sll.push(3);
+        sll.push(5);
+        sll.push(9);
+        sll.push(1);
+        sll.push(43);
+        System.out.println(sll.head.data);
+        sll.printList(method1(sll.head));
     }
 
 }
--- DSA/LinkedList/src/ReverseLL.java
+++ DSA/LinkedList/src/ReverseLL.java
@@ -1,10 +1,26 @@
+/**
+ * <h1>Reverse linked list</>
+ * https://leetcode.com/problems/reverse-linked-list/
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ *
+ * @author Ishita Bhardwaj
+ * @version 11.01
+ * @since   2021-05-27
+ * */
 public class ReverseLL {
 
-    /**
-     * space O(n)
-     * time O(n)
-     * */
     public static SLL.Node myMethod(SLL.Node head) {
+        /**
+         * This method has a Node Array of max size
+         * As we'll traverse the list we'll save the pointers of the whole list
+         * As we reach the end of the list with size i-1, we'll appoint the last index as head and
+         * traverse the Node Array in the reverse direction while appointing the next node of the iterator
+         * and end the list with a null
+         * space complexity: O(n)
+         * time complexity: O(n)
+         * @param (SLL.Node head) The parameter is used to point at the head of an input linked list
+         * @return SLL.Node returns the head pointer of the reversed linked list.
+         */
         SLL.Node[] A = new SLL.Node[5000];
         SLL.Node temp = head;
         int i = 0;
@@ -12,9 +28,7 @@ public class ReverseLL {
             A[i++] = temp;
             temp = temp.next;
         }
-
         head = A[i-1];
-        System.out.println(head);
         temp = head;
         int size = i-2;
         while(size >= 0) {
@@ -30,24 +44,48 @@ public class ReverseLL {
      * time O(n)
      * */
     public static SLL.Node method1(SLL.Node head) {
+        /**
+         * This method has two pointers and the parameter head pointer
+         * We'll the save the next node to the head
+         * Put the previous node as next to the head
+         * Iterate the prev to the head pointer
+         * Iterate the head to the temp pointer
+         * space complexity: O(1)
+         * time complexity: O(n)
+         * @param (SLL.Node head) The parameter is used to point at the head of an input linked list
+         * @return SLL.Node returns the head pointer of the reversed linked list.
+         */
         if(head == null || head.next == null)
             return head;
         SLL.Node temp = head, prev = null;
         while(temp.next != null) {
-            temp = head.next;
-            head.next = prev;
-            prev = head;
-            head = temp;
+            temp = head.next;//save the next node
+            head.next = prev;//make the next to head node as prev
+            prev = head;//iterate prev to head
+            head = temp;//iterate head to temp which was saved initially
         }
         head.next = prev;
         return head;
     }
 
-    /**
-     * space O(n)
-     * time O(n)
-     * */
     public static SLL.Node method2(SLL.Node head) {
+        /**
+         * This is a recursive method
+         * We have a base condition if the list is empty or have only one node then return the head itself
+         * We take a temporary pointer and apply recursion for the node next to the current pointer
+         * As the recursive functions unwrap, we simply do two things:
+         * 1 --> pass down the head of the reversed linked list i.e the last node
+         * 2 --> reverse the pointer of the two current nodes (adjacent) from
+         * Step 2.1: [a1(head) --> a2(head.next) --> null(head.next.next)]
+         * Step 2.2: [a1(head) --> a2(head.next) --> a1(head.next.next == head)]
+         * Step 2.3: [a1(head) --> null(head.next) | a2(head.next) --> a1(head.next.next == head) --> null]
+         * Simplified 2.3: a2(head.next) --> a1(head.next.next == head) --> null
+         * again here head only represents the head in the current stack; last node is still the all over head being passed down
+         * space complexity: O(n)
+         * time complexity: O(n)
+         * @param (SLL.Node head) The parameter is used to point at the head of an input linked list
+         * @return SLL.Node returns the head pointer of the reversed linked list.
+         */
         if(head == null || head.next == null) {
             return head;
         }
