b992a181989953b1cb72407864fbfdf8b186d6c8
Ishita <bhardwajishita15@gmail.com>
2021-05-31 22:01:32 +0530

    added a new method to ReorderList.class

--- DSA/LinkedList/src/ReorderList.java
+++ DSA/LinkedList/src/ReorderList.java
@@ -102,6 +102,60 @@ public class ReorderList {
     }
 
     /**
+     * This method uses <b>recursion</b>.
+     * Hold a pointer to the head node and go till the last node using recursion
+     * Once the last node is reached, start swapping the last node to the next of head node
+     * Move the head pointer to the next node
+     * Repeat this until the head and the last node meet or come adjacent to each other
+     * Once the Stop condition met, we need to discard the left nodes to fix the loop created in the list while swapping nodes.
+     * space complexity: O(1)
+     * time complexity: O(n)
+     * @param head The parameter is used to point at the head of an input linked list
+     * @return void You have to make changes in the given linked list without changing the head
+     */
+
+    static SLL.Node left = null;//announce the left node globally to use in the rearrange method
+    //because you need to save the head pointer and you can't do that without appointing another pointer to it
+    // that can be used in both the methods
+
+    public static void method3(SLL.Node head) {
+        if(head != null) {
+            left = head;
+            rearrange(left);
+        }
+    }
+
+    public static void rearrange(SLL.Node right) {
+        if(right == null)
+            return;
+
+        SLL.Node prev = right;
+        rearrange(right.next);//to reach the tail of the list
+        //if(right != null && left != null) System.out.println("prev: " + prev.data + " right: " + right.data + " left: " + left.data);
+
+        if(left == null)
+            return;
+
+        //when the left node is behind the right node: for both odd and even nodes
+        //if odd nodes (left == right) elseif even nodes (left.next == right)
+        if(left != right && left.next != right) {
+            SLL.Node temp = left.next;
+            left.next = right;
+            right.next = temp;
+            left = temp;
+        }
+        //when the right and left nodes have reached the base condition
+        else if(left == right || left.next == right) {
+            right.next = null;
+            left = null;
+        }
+        else {
+            left = null;
+        }
+
+    }
+
+    /**
      * This is the main method which makes use of addNum method.
      * @param args Unused.
      */
@@ -113,7 +167,7 @@ public class ReorderList {
         sll.push(3);
         sll.push(2);
         sll.push(1);
-        method2(sll.head);
+        method3(sll.head);
         sll.printList(sll.head);
     }
 }
