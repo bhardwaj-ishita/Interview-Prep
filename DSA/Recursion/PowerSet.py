c2a534c671ec86344df4789776699005c4bfd1f1
Ishita Bhardwaj <68750732+bhardwaj-ishita@users.noreply.github.com>
2022-07-30 13:43:05 +0530

    Create PowerSet.py

---
+++ DSA/Recursion/PowerSet.py
@@ -0,0 +1,25 @@
+'''
+Here in the question is asked to find all subsequences of the string
+And print them in lexicographical order
+
+Why I have added a pop funciton at the end?
+Because the last subsequence added in the list is an empty string and that is not a power set
+So I popped it and sorted it and returned
+'''
+
+class Solution:
+	def AllPossibleStrings(self, s):
+		# Code here
+		ans = self.sol(s,len(s), [], "")
+		ans.pop()
+		ans.sort()
+		return ans
+	def sol(self, s, n, ans, temp):
+	    if n==0:
+	        ans.append(temp)
+	        return ans
+	    
+	    self.sol(s,n-1,ans, s[n-1] + temp)
+	    self.sol(s,n-1,ans, temp)
+	    
+	    return ans
