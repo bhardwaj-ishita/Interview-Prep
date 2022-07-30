8dc57673724bca835ae55770df7eda43d15275e6
Ishita Bhardwaj <68750732+bhardwaj-ishita@users.noreply.github.com>
2022-07-30 13:48:22 +0530

    Update PowerSet.py

--- DSA/Recursion/PowerSet.py
+++ DSA/Recursion/PowerSet.py
@@ -5,6 +5,12 @@ And print them in lexicographical order
 Why I have added a pop funciton at the end?
 Because the last subsequence added in the list is an empty string and that is not a power set
 So I popped it and sorted it and returned
+
+
+And one thing you'll notice when you draw the recursion tree of this question is that all the unique subsequences can be found at the leaf nodes.
+So no need to append the strings in the middle. Add them when you reach the leaf node.
+And their is a choice in this question, you either add the char at ith position or not
+
 '''
 
 class Solution:
