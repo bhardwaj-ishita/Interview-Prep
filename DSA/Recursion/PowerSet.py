6e5eb2eb6ee7117e212866253726d6cbea6c04ad
Ishita Bhardwaj <68750732+bhardwaj-ishita@users.noreply.github.com>
2022-08-02 07:43:31 +0530

    Update PowerSet.py

--- DSA/Recursion/PowerSet.py
+++ DSA/Recursion/PowerSet.py
@@ -29,3 +29,51 @@ class Solution:
 	    self.sol(s,n-1,ans, temp)
 	    
 	    return ans
+	
+'''
+"array nums of unique elements" it is given in the question that the input has unique elements in it.
+So we don't need to take care of duplicates
+We just need to print. Hence we can use a list
+
+A set would've taken care of duplicates
+
+Lists
+
+- ordered
+- mutable
+- [a,b,c]
+- it is an object which contains any kind of object, like, a list, a set, diff data types etc
+
+Tuples
+
+- ordered
+- immutable
+- once in a lifetime of the code it is created: once created, it remains intact
+- (a,b,c)
+- prevents accidental addition or deletion of the data
+- use less memory and program execution faster than list, as every time a new execution is done with lists, new objects are created, and the objects are not interpreted just once
+
+Sets
+
+- unique in the use case; mostly used to take care of duplicates
+- mutable
+- {a,b,c}
+- used to check whether a set is a subset of another set and to identify the relationship between two sets
+'''	
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        self.sol(nums,len(nums),ans,[])
+            
+        return ans
+        
+    def sol(self, a, n, ans, temp):
+        if n == 0:
+            ans.append(temp)
+            return ans
+        
+        self.sol(a,n-1,ans,temp)
+        self.sol(a,n-1,ans,[a[n-1]] + temp)
+        
+        
+        return ans
