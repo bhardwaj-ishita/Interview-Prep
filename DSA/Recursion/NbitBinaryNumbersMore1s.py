5083b3869d1bc9f8a8c66597117f8a79cade204e
Ishita Bhardwaj <68750732+bhardwaj-ishita@users.noreply.github.com>
2022-08-03 11:09:21 +0530

    Update NbitBinaryNumbersMore1s.py

--- DSA/Recursion/NbitBinaryNumbersMore1s.py
+++ DSA/Recursion/NbitBinaryNumbersMore1s.py
@@ -24,3 +24,26 @@ class Solution:
         
         
         return ans
+
+
+#Used zero and one instead of count variable. Now the condition is one >= zero
+class Solution:
+	def NBitBinary(self, N):
+		# code here
+		ans = []
+		self.sol(0,ans,"",0,0,N)
+		return ans
+		
+	def sol(self,n,ans,temp,zero,one,N):
+	    if n == N:
+	        if one >= zero:
+	            ans.append(temp)
+            return ans
+        
+        
+        if one >= zero:
+            self.sol(n+1,ans,temp+"1",zero,one+1,N)
+            self.sol(n+1,ans,temp+"0",zero+1,one,N)
+        
+        
+        return ans
