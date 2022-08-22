43c46e0427f481ac25de76f09a58f783efc3e3a5
Ishita Bhardwaj <68750732+bhardwaj-ishita@users.noreply.github.com>
2022-08-22 09:43:16 +0530

    Update DetectCycleDirected.py

--- DSA/Graphs/DetectCycleDirected.py
+++ DSA/Graphs/DetectCycleDirected.py
@@ -1,3 +1,17 @@
+'''
+The thing is when we only use visited, we don't know why a node has been marked visited
+Mabye because while traversing a continuously linked set of nodes or maybe it was visited before
+but it pointed somewhere else, like 5->4->4->3->2->1->0 , hence it is marked visited
+Now here 0 will be visited first and nothing to point towards
+
+Now for recStack we mark true for all the nodes in one traversal, and if you end up again 
+on an already visited node, then it is true
+But if you eneded up on a node which is visited already, but never traversed, 
+it means that, that the next node in traversal is this particular node, and when we traversed it, we didn't
+find any loop then why do you think when you'll get on the same node again, you'll find one
+, so break out of the traversal already because you're at a dead end.
+
+'''
 class Solution:
     
     #Function to detect cycle in a directed graph.
