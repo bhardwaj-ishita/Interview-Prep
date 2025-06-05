b5e05c4caa5f1457cf025719800b25bab450842b
Ishita Bhardwaj <68750732+bhardwaj-ishita@users.noreply.github.com>
2025-06-05 21:13:42 +0530

    Update UndirectedBFSCycle.py

--- DSA/Graphs/UndirectedBFSCycle.py
+++ DSA/Graphs/UndirectedBFSCycle.py
@@ -3,23 +3,23 @@ class Solution:
 	def isCycle(self, V, edges):
 		#Code here
 		
-        #for bfs the queue needs to have a parent node in it, so specify it in the function if you're creating one. else it fails for a disconnected graph            
-        #and it should be filled with the parent of the new disconnected graph
-        def bfs(node):
-            q.append(node)
-		    visited[node] = 1
-		    parent[node] = -1
-            while(q):
-                node = q.popleft()
-                for neigh in graph[node]:
-                    if visited[neigh] == 0:
-                        visited[neigh] = 1
-                        parent[neigh] = node
-                        q.append(neigh)
-                    else:
-                        if visited[neigh] == 1 and parent[node] != neigh:
-                            return True
-            return False
+		#for bfs the queue needs to have a parent node in it, so specify it in the function if you're creating one. else it fails for a disconnected graph            
+		#and it should be filled with the parent of the new disconnected graph
+		def bfs(node):
+		    q.append(node)
+			    visited[node] = 1
+			    parent[node] = -1
+		    while(q):
+			node = q.popleft()
+			for neigh in graph[node]:
+			    if visited[neigh] == 0:
+				visited[neigh] = 1
+				parent[neigh] = node
+				q.append(neigh)
+			    else:
+				if visited[neigh] == 1 and parent[node] != neigh:
+				    return True
+		    return False
             
 		
 		graph = [[] for _ in range(V)]
@@ -33,7 +33,7 @@ class Solution:
 		 
 		for node in range(V):
 		    if visited[node] == 0:
-		        ans = bfs(node)
-		        if ans:
-		            return True
+			ans = bfs(node)
+			if ans:
+			    return True
 		return False
